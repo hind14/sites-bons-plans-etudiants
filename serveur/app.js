4c3cedf092297f8e73af65d0bbd1ae659f4760f6
hind14 <hind.elgharbi@live.fr>
2023-05-25 10:24:40 +0200

    fix

--- serveur/app.js
+++ serveur/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Hello" });
 })
 
 
