b837a83e7c2893ba666acf008f78d983c00b0469
hind14 <hind.elgharbi@live.fr>
2023-05-25 10:09:49 +0200

    adding bodyparser + correction

--- serveur/app.js
+++ serveur/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const cors = require("cors")
 
 const app = express()
@@ -9,14 +10,15 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome" });
+  res.json({ message: "Welcome to bezkoder application." });
 })
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
